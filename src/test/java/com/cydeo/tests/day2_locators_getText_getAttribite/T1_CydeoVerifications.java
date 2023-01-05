f9c61d4cf2ddf5dc986d502d61661f88e9468aaa
Mehmet <meh.korucu@gmail.com>
2023-01-06 02:05:47 +0300

    T1_CydeoVerification

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribite/T1_CydeoVerifications.java
@@ -0,0 +1,33 @@
+package com.cydeo.tests.day2_locators_getText_getAttribite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_CydeoVerifications {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://practice.cydeo.com");
+
+        String actualUrl=driver.getCurrentUrl();
+
+        if(actualUrl.contains("cydeo")){
+            System.out.println("T1 PASSED");
+        }else{
+            System.out.println("T1 FAILED");
+        }
+
+        String actualTitle=driver.getTitle();
+
+        //Expected Title=Practice
+        String expectedTitle="Practice";
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("T2 PASSED");
+        }else{
+            System.out.println("T2 FAILED");
+        }
+    }
+}
