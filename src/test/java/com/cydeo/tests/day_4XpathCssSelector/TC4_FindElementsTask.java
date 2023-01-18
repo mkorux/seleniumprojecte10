15b9b665a17303977d572b1ff3865dd49eb5d9e6
Mehmet <meh.korucu@gmail.com>
2023-01-19 01:09:50 +0300

    Day4_TC4Study

--- src/test/java/com/cydeo/tests/day_4XpathCssSelector/TC4_FindElementsTask.java
+++ src/test/java/com/cydeo/tests/day_4XpathCssSelector/TC4_FindElementsTask.java
@@ -2,8 +2,13 @@ package com.cydeo.tests.day_4XpathCssSelector;
 
 import com.cydeo.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TC4_FindElementsTask {
@@ -16,10 +21,19 @@ public class TC4_FindElementsTask {
         //2- Go to: https://practice.cydeo.com/abtest
         driver.get("https://practice.cydeo.com/abtest ");
 
-        //3- Locate all the links in the page.
+        /* 3- Locate all the links in the page. */
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
 
         //4- Print out the number of the links on the page.
+        System.out.println("allLinks.size() = " + allLinks.size());
+
         //5- Print out the texts of the links.
+        for (int i = 0; i < allLinks.size(); i++) {
+            System.out.println(allLinks.get(i));
+        }
         //6- Print out the HREF attribute values of the links
+        for (WebElement each:allLinks) {
+            System.out.println(each.getAttribute("href"));
+        }
     }
 }
