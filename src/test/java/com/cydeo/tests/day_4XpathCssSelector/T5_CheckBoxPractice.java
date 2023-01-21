5ed99074aa79028942a9a182a122793e338231f7
Mehmet <meh.korucu@gmail.com>
2023-01-22 00:04:53 +0300

    Day4_TC5_CheckBoxPractice

--- src/test/java/com/cydeo/tests/day_4XpathCssSelector/T5_CheckBoxPractice.java
+++ src/test/java/com/cydeo/tests/day_4XpathCssSelector/T5_CheckBoxPractice.java
@@ -19,7 +19,7 @@ public class T5_CheckBoxPractice {
 
         //2. Confirm checkbox #1 is NOT selected by default
         WebElement checkbox1=driver.findElement(By.xpath("//input[@id='box1']"));
-        if(checkbox1.isDisplayed()){
+        if(checkbox1.isSelected()){
             System.out.println("FAILED");
         }else{
             System.out.println("PASSED");
@@ -27,27 +27,32 @@ public class T5_CheckBoxPractice {
 
         //3. Confirm checkbox #2 is SELECTED by default.
         WebElement checkbox2=driver.findElement(By.xpath("//input[@id='box2']"));
-        if(checkbox2.isDisplayed()){
+        if(checkbox2.isSelected()){
             System.out.println("PASSED");
         }else{
             System.out.println("FAILED");
         }
 
         //4. Click checkbox #1 to select it.
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         checkbox1.click();
 
         //5. Click checkbox #2 to deselect it.
         checkbox2.click();
 
         //6. Confirm checkbox #1 is SELECTED.
-        if(checkbox1.isDisplayed()){
+        if(checkbox1.isSelected()){
             System.out.println("PASSED");
         }else{
             System.out.println("FAILED");
         }
 
         //7. Confirm checkbox #2 is NOT selected.
-        if(checkbox2.isDisplayed()){
+        if(checkbox2.isSelected()){
             System.out.println("FAILED");
         }else{
             System.out.println("PASSED");
