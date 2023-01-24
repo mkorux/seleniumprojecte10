e2273d6540d77732d0a7351bc96cc423b0cc7730
Mehmet <meh.korucu@gmail.com>
2023-01-24 22:35:16 +0300

    Day5_TC2

--- src/test/java/com/cydeo/tests/day5_testNGIntro_dropdowns/T2_RadionBtnHandling.java
+++ src/test/java/com/cydeo/tests/day5_testNGIntro_dropdowns/T2_RadionBtnHandling.java
@@ -2,7 +2,9 @@ package com.cydeo.tests.day5_testNGIntro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import com.google.common.base.Verify;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,8 +15,19 @@ public class T2_RadionBtnHandling {
         WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         //2. Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
         //3. Click to “Hockey” radio button
+        WebElement hockeyBtn=driver.findElement(By.xpath("//input[@id='hockey']"));
+        hockeyBtn.click();
+
         //4. Verify “Hockey” radio button is selected after clicking.
+        if (hockeyBtn.isSelected()){
+            System.out.println("Hockey btn is selected; TRUE");
+        }else {
+            System.out.println("Hockey btn is selected; FALSE");
+        }
     }
 }
